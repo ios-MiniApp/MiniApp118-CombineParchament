d39ead79da554eba54eaf693607f656ed0b531a4
Kota <maedakoutaa@gmail.com>
2023-04-28 10:24:54 +0900

    Initial Commit

---
+++ MiniApp118-CombineParchament/MiniApp118_CombineParchamentApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniApp118_CombineParchamentApp.swift
+//  MiniApp118-CombineParchament
+//
+//  Created by 前田航汰 on 2023/04/28.
+//
+
+import SwiftUI
+
+@main
+struct MiniApp118_CombineParchamentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MiniApp118-CombineParchamentUITests/MiniApp118_CombineParchamentUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MiniApp118_CombineParchamentUITestsLaunchTests.swift
+//  MiniApp118-CombineParchamentUITests
+//
+//  Created by 前田航汰 on 2023/04/28.
+//
+
+import XCTest
+
+final class MiniApp118_CombineParchamentUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
